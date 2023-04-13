d14da0165cbec12b91f1fb7dc10df3e48508598f
Napoleon Ahiable <219075+thepreacher@users.noreply.github.com>
2023-04-13 10:00:56 +0100

    chezmoi unmanage zsh

--- .iex.exs
+++ .iex.exs
@@ -9,11 +9,11 @@ IEx.configure(
   ],
   default_prompt:
     "#{IO.ANSI.green()}%prefix#{IO.ANSI.reset()}" <>
-      "(#{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()})>",
+      "(#{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()})#{IO.ANSI.green()}❯#{IO.ANSI.reset()}",
   alive_prompt:
     "#{IO.ANSI.green()}%prefix#{IO.ANSI.reset()}" <>
       "[#{IO.ANSI.yellow()}%node#{IO.ANSI.reset()}]" <>
-      "(#{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()})>",
+      "(#{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()})#{IO.ANSI.green()}❯#{IO.ANSI.reset()}",
   history_size: 50,
   inspect: [
     pretty: true,
