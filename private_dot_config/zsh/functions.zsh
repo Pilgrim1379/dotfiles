a63b15cf4fb6e66d5f1a3adec1ec83931e7b80b2
Napoleon Ahiable <219075+Pilgrim1379@users.noreply.github.com>
2024-09-17 08:31:04 +0100

    switch zsh to zsh4humans

---
+++ private_dot_config/zsh/functions.zsh
@@ -0,0 +1,42 @@
+## Neovim config switcher
+function nvims() {
+	items=(
+		"default"
+		"LazyVim"
+	)
+	config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config 󰄾 " --height=~50% --layout=reverse --border --exit-0)
+	if [[ -z $config ]]; then
+		echo "Nothing selected"
+		return 0
+	elif [[ $config == "default" ]]; then
+		config=""
+	fi
+	NVIM_APPNAME=$config nvim $@
+}
+
+# Search through all man pages
+function fman() {
+	man -k . | fzf -q "$1" --prompt='man> ' --preview $'echo {} | tr -d \'()\' | awk \'{printf "%s ", $2} {print $1}\' | xargs -r man' | tr -d '()' | awk '{printf "%s ", $2} {print $1}' | xargs -r man
+}
+
+# Improved myip alias. Echoed to avoid strange character at end in ZSH.
+function myip() {
+	echo "$(curl -s whatismyip.akamai.com)"
+}
+
+# Python PDM
+function pdm() {
+	local command=$1
+
+	if [[ "$command" == "shell" ]]; then
+		eval $(pdm venv activate)
+	else
+		command pdm $@
+	fi
+}
+
+# Make and cd into directory
+#  - from: http://alias.sh/make-and-cd-directory
+function mkcd() {
+	mkdir "$1" && cd "$1" || exit
+}
---
+++ private_dot_config/zsh/paths.zsh
@@ -0,0 +1,63 @@
+
+# Shellenv
+# if (( $+commands[brew] )); then
+#     eval "$(brew shellenv)"
+# fi
+
+# $PATH and $path (and also $FPATH and $fpath, etc.) are "tied" to each other.
+# Modifying one will also modify the other.
+# Note that each value in an array is expanded separately. Thus, we can use ~
+# for $HOME in each $path entry.
+
+path=(
+    $HOME/.nimble/bin(N) # Nim
+    $HOME/.cargo/bin(N) # Rust
+    $HOME/.juliaup/bin(N) # Julia
+    $HOME/go/bin(N) # Golang
+    $HOME/github/language-servers/lexical/bin(N) # Lexical
+    $HOME/github/language-servers/elixir-ls(N) # Elixir
+    $HOME/github/language-servers/next-ls(N) # Nextls
+    /Applications/Postgres.app/Contents/Versions/latest/bin(N) # Postgress app
+    $HOME/Library/Application\ Support/Coursier/bin(N) # Scala
+    $HOME/Library/Android/sdk/{emulator,platform-tools,tools}(N)
+    $HOMEBREW_PREFIX/opt/{mariadb@10.3,ncurses,tomcat@9}/bin(N)
+    $HOMEBREW_PREFIX/opt/{coreutils/libexec/gnubin,findutils/libexec/gnubin,grep/libexec/gnubin}/bin(N)
+    $HOMEBREW_PREFIX/opt/{curl,gettext,sqlite,llvm}/bin(N)
+    # /home/linuxbrew/.linuxbrew/bin(N)
+    $path
+)
+
+# +-------------------+
+# | Ocaml |
+# +-------------------+
+[[ ! -r ~/.opam/opam-init/init.zsh ]] || source ~/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
+
+# +-------------------+
+# | Haskell ghcup-env |
+# +-------------------+
+[ -f ~/.ghcup/env ] && source ~/.ghcup/env
+
+# Bun path
+path=(
+    $HOME/.bun/bin(N)
+    $path
+)
+
+########################################################################################################
+## This should come after all essential PATHs has been set unless you know what you're doing          ##
+########################################################################################################
+if (( $+commands[mise] )); then
+    export MISE_USE_TOML=1
+    # export MISE_DATA_DIR=$XDG_DATA_HOME/mise
+    export MISE_EXPERIMENTAL=1
+    # export MISE_PYTHON_COMPILE=1
+    # export MISE_ASDF_COMPAT=1
+    # export MISE_DEBUG=1
+    # export MISE_LOG_LEVEL=debug
+    # export RUST_BACKTRACE=1
+
+    # eval "$(mise activate zsh --shims)" # should be first if using mise shims and disable line with mise hook-env
+    eval "$(mise activate zsh)"
+    eval "$(mise hook-env -s zsh)" # enable this if not using mise shims for sublime text to work
+fi
+########################################################################################################
